e38023e78c2b3591c03905d25e2ee7db741cf1b1
mayuriyam <mayuriya21@gmail.com>
2017-06-30 18:12:23 +0530

    Create cntwrd.java

---
+++ cntwrd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class cntwrd
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+       String a=sc.nextLine();
+       String b[]=a.split(" ");
+       System.out.println(b.length);
+        
+       
+    }
+}
+
